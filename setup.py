4225833a13d75c85a58b3f636249ee64aacdf99e
Katherine Temkin <k@ktemkin.com>
2021-03-02 10:32:52 -0700

    drop nmigen versioning to allow use of master

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     license="BSD",
     setup_requires=["setuptools_scm"],
     install_requires=[
-        "nmigen>=0.1,<0.3",
+        "nmigen",
     ],
     entry_points={
         "console_scripts": [
